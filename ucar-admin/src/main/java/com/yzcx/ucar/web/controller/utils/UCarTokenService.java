eee3ccd1cde336362b0bf6ff6011682f5c0f876e
yuzhenqing <yuzq@0791jx.cn>
2020-12-24 12:41:12 +0800

    完善地图

--- ucar-admin/src/main/java/com/yzcx/ucar/web/controller/utils/UCarTokenService.java
+++ ucar-admin/src/main/java/com/yzcx/ucar/web/controller/utils/UCarTokenService.java
@@ -1,25 +1,13 @@
 package com.yzcx.ucar.web.controller.utils;
 
-import com.alibaba.fastjson.JSONObject;
 import com.yzcx.ucar.common.utils.StringUtils;
 import com.yzcx.ucar.web.controller.utils.entity.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.security.MessageDigest;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * author: yuzq
@@ -48,19 +36,11 @@ public class UCarTokenService {
     }*/
 
     @RequestMapping("queryDayTravelStr")
-    public List<DayTravelStr> queryDayTravelStr(@RequestBody StrEntity strEntity) {
-        if(StringUtils.isEmpty(strEntity.getDeviceId())){
+    public List<DayTravelStr> queryDayTravelStr(@RequestBody StrEntity strEntity) throws ParseException {
+        if(StringUtils.isEmpty(strEntity.getDeviceId()) || StringUtils.isEmpty(strEntity.getTime())){
             return null;
         }
-        String format1="";
-        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            Date parse = format.parse(strEntity.getTime());
-            format1 = format.format(parse);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return TokenUtil.queryDayTravelStr(strEntity.getDeviceId(),format1);
+        return TokenUtil.queryDayTravelStr(strEntity.getDeviceId(),strEntity.getTime());
     }
 
     @RequestMapping("queryTravelHistory")
